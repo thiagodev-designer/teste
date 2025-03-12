8a6f709304368e94514f786b9c57821cc39cdfbf
thiagodev-designer <ferreira.clarodf@gmail.com>
2025-03-12 18:37:13 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize('menssagens','root','',{
-    host: 'localhost',
+    host: 'http://localhost:8282/phpmyadmin/',
   dialect: 'mysql'
 })
 
